87b2981d753f192b1368570174455363816797ea
Oleg Biblyi <oleg_biblyi@hotmail.com>
2020-12-15 22:15:51 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Route, Switch } from "react-router-dom";
 import { Header } from "./layouts";
+import { LandingPage } from "./pages";
 
 const App = () => {
   return (
     <>
       <Header />
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+      </Switch>
     </>
   );
 };
