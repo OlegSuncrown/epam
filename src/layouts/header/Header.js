e4f16c60c7ca9e953f16e92c0611664d4c09369b
Oleg Biblyi <oleg_biblyi@hotmail.com>
2020-12-15 20:05:58 +0200

    add font-awsome

--- src/layouts/header/Header.js
+++ src/layouts/header/Header.js
@@ -1,18 +1,22 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 const Header = () => {
   return (
     <div>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-        <Navbar.Brand href="#">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#">Home</Nav.Link>
-            <Nav.Link href="#">Link</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+      <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="#">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="#">Home</Nav.Link>
+              <Nav.Link href="#">
+                <i className="fas fa-user"></i>Link
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     </div>
   );
