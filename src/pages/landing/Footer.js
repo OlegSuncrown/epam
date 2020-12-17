e83c44b177d3908ea515b5c9f3f3111f538832b3
cmpljs <cmplcmplcmpl@gmail.com>
2020-12-17 16:50:25 +0200

    c

--- src/pages/landing/Footer.js
+++ src/pages/landing/Footer.js
@@ -1,9 +1,29 @@
 import React from "react";
-import { Container, Col, Button, Row } from "react-bootstrap";
+import { Container, Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Logo from "../../assets/logo.svg";
+import instagramIcon from "../../assets/instagramIcon.svg";
+import facebookIcon from "../../assets/facebookIcon.svg";
 
 const Footer = () => {
-  return <></>;
+  return (
+    <Container fluid className="p-5 footer">
+      <Row>
+        <Col>
+          <img src={Logo}></img>
+        </Col>
+        <Col>How it works</Col>
+        <Col>About us</Col>
+        <Col>Terms of use</Col>
+        <Col>
+          <img src={instagramIcon}></img>
+        </Col>
+        <Col>
+          <img src={facebookIcon}></img>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Footer;
