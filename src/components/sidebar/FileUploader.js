a4661277a65bb18773054f3bef1776e87e1ba1dc
cmpljs <cmplcmplcmpl@gmail.com>
2020-12-18 17:16:08 +0200

    c

--- src/components/sidebar/FileUploader.js
+++ src/components/sidebar/FileUploader.js
@@ -1,13 +1,14 @@
 import React from "react";
 
-const FileUploader = () => {
+const FileUploader = (props) => {
   return (
     <>
       <form>
-        <label className="uploadButton" for="upload-photo">
+        <label className="uploadButton" htmlFor="upload-photo">
           <strong className="h5">Upload avatar</strong>
         </label>
         <input
+          onChange={props.handleSubmit()}
           type="file"
           name="photo"
           id="upload-photo"
--- src/components/sidebar/UserAvatar.js
+++ src/components/sidebar/UserAvatar.js
@@ -1,10 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Card, Image } from "react-bootstrap";
 import { AuthContext } from "../../context/auth/AuthContext";
 import FileUploader from "./FileUploader";
 
 const UserAvatar = () => {
-  const { user } = useContext(AuthContext);
+  const { user, axios } = useContext(AuthContext);
+  const [avatar, setAvatar] = useState("");
+  const URL = "https://hwtaweb20201216131958.azurewebsites.net";
+
+  //to be refactored
+  const updateAvatar = async (event) => {
+    setAvatar(event.target.files[0]);
+
+    try {
+      const { data } = await axios.post(`${URL}/api/User`, {
+        body: avatar,
+      });
+
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <Card className="border-light text-center d-none d-lg-block shadow pb-3">
@@ -20,7 +37,7 @@ const UserAvatar = () => {
           <h2 className="text-secondary">
             <strong>{user ? user.firstName : "loading..."}</strong>
           </h2>
-          <FileUploader />
+          <FileUploader handleSubmit={updateAvatar} />
         </Card.Title>
       </Card.Body>
     </Card>
