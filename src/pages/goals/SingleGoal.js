b9fc0f476d20f31c1c24dcaaf26009585eaff51a
Tanya <saputo1990@gmail.com>
2020-12-19 23:10:30 +0200

    fixed bugs

--- src/pages/goals/SingleGoal.js
+++ src/pages/goals/SingleGoal.js
@@ -1,10 +1,11 @@
 import React, { useState, useContext } from "react";
-import { Container, Button, Row, Form, Col, Dropdown } from "react-bootstrap";
+import { Button, Row, Form, Col, Dropdown } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import swal from "sweetalert2";
 import { BrowserRouter as Router, Link, useLocation } from "react-router-dom";
 import { DefaultGolasContext } from "../../context/default/DefaultGoalsContext";
 import axios from "axios";
+import setAuthToken from "../../utils/setAuthToken";
 
 const SingleGoal = () => {
   const { register, handleSubmit } = useForm();
@@ -21,6 +22,10 @@ const SingleGoal = () => {
       headers: { "Content-Type": "application/json" },
     };
 
+    if (localStorage.AuthToken) {
+      setAuthToken(localStorage.AuthToken);
+    }
+
     try {
       await axios.post(`${URL}/addNewUserGoal`, formData, config);
       swal.fire("Success", "Goal was successfully added!", "success");
@@ -34,6 +39,11 @@ const SingleGoal = () => {
   };
 
   const onSubmit = (data, e) => {
+    data.value = parseInt(data.value);
+    data.startDate = new Date(data.startDate).toISOString();
+    data.endDate = new Date(data.endDate).toISOString();
+    data = { ...data, ...{ regularty: 0 } };
+    console.log(data);
     postGoal(data);
     e.target.reset();
   };
@@ -48,15 +58,6 @@ const SingleGoal = () => {
     goal = dataList.find((goal) => goal.title === goalTitle);
   }
 
-  if (isNew && !goal) {
-    return (
-      <Container>
-        <Row className="justify-content-md-center pt-2">
-          <h1>This goal does not exist</h1>
-        </Row>
-      </Container>
-    );
-  }
   return (
     <div className="d-flex justify-content-center h-100">
       <Form
@@ -68,30 +69,57 @@ const SingleGoal = () => {
             <Form.Row>
               <Form.Label htmlFor="title">Add cutom goal name</Form.Label>
               <Form.Control
-                name="title"
+                placeholder="Enter goal name here"
+                name="goalTitle"
                 ref={register}
                 required
                 size="lg"
-                placeholder="Enter goal name here"
                 type="text"
-                id="title"
+                id="goalTitle"
               />
             </Form.Row>
           ) : (
             <Form.Row>
               <Form.Control
                 className="border-0 text-uppercase"
-                name="title"
+                defaultValue={goal.title}
+                name="goalTitle"
                 ref={register}
                 required
                 size="lg"
-                defaultValue={goal.title}
                 type="text"
                 id="title"
               />
             </Form.Row>
           )}
         </Form.Row>
+        {isNew ? (
+          <Form.Row>
+            <Form.Label htmlFor="title">Add cutom goal description</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              placeholder="Enter goal description here"
+              name="description"
+              ref={register}
+              required
+              size="lg"
+            />
+          </Form.Row>
+        ) : (
+          <Form.Group>
+            <Form.Control
+              as="textarea"
+              className="border-0"
+              defaultValue={goal.description}
+              name="description"
+              ref={register}
+              required
+              size="lg"
+            />
+          </Form.Group>
+        )}
+
         <Form.Row>
           <Form.Group as={Col} md="6" controlId="validationCustom01">
             <Form.Label>Select Start date</Form.Label>
@@ -131,11 +159,11 @@ const SingleGoal = () => {
           </Form.Group>
           {isNew ? (
             <Form.Group as={Col} md="6" controlId="validationCustom02">
-              <Form.Label>Measure</Form.Label>
+              <Form.Label>Enter type of value</Form.Label>
               <Form.Control
                 required
                 type="text"
-                name="measure"
+                name="valueType"
                 ref={register}
                 size="lg"
               />
@@ -147,7 +175,7 @@ const SingleGoal = () => {
                 className="border-0 text-uppercase mt-4 text-center"
                 required
                 type="text"
-                name="measure"
+                name="valueType"
                 defaultValue={goal.item}
                 ref={register}
                 size="lg"
